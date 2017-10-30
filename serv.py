044e86ad0986b27c156db68d01e09552edbd6fff
TeRRy <k8510232001@gmail.com>
2017-10-30 10:40:32 +0800

    first commit

---
+++ serv.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/plusone/<int:num>')
+def plusone(num):
+	return '%d' % (num + 1)
+	
+@app.route('/request',method=['GET' ,'POST'])
+def processreq():
+	if request.method == 'GET':
+		return "Get key: %s,val: %s % (request.args.get('key'),request.args.get('val'))
+	else
+		return "Post key: %s,val: %s % (request.form['key'],request.form['val'])
+
+@app.route('/postpage/')
+@app.route('/postpage/<pname>')
+def postpage(pname = "No_name"):
+	return render_template('postform.html',name = pname)
+	
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0',debug = true)
+    
